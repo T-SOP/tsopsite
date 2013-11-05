69c9e3ca1eb4675caacb77c3841ac9dab21fca5b
bongsungju <bongster88@gamil.com>
2013-11-05 16:33:28 +0900

    insert Google + Login and Modal

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,10 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  val CLIENT_ID = "425874550737-rcq17uufrfphfdpl1sjdpb57331cgor4.apps.googleusercontent.com";
+  val CLIENT_SECRET = "tB1w4J_r3bJa_bo6vgFiXcS6"
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(CLIENT_ID))
   }
 
 }
